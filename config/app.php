1f9080c1e836a3c30029def08ed705ce3b8c909f
Provost Digital <pods@Provosts-MacBook-Pro-2.local>
2019-06-24 13:50:00 -0400

    Add monitor model and cron job

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Spatie\UptimeMonitor\UptimeMonitorServiceProvider::class,
 
         /*
          * Application Service Providers...
