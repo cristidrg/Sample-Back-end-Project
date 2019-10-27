550f710589b5486fac9c38bb56bcde333219353a
Provost Digital <pods@Provosts-MacBook-Pro-2.local>
2019-10-27 01:51:50 -0400

    Remove old CSS from sidebar and app

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,16 +1,9 @@
 @php
     isset($q) ? '' : $q = '';
-    isset($a11y_green) ? '' : $a11y_green = 'false';
-    isset($a11y_yellow) ? '' : $a11y_yellow = 'false';
-    isset($a11y_red) ? '' : $a11y_red = 'false';
-    isset($seo_green) ? '' : $seo_green = 'false';
-    isset($seo_yellow) ? '' : $seo_yellow = 'false';
-    isset($seo_red) ? '' : $seo_red = 'false';
-    isset($perf_green) ? '' : $perf_green = 'false';
-    isset($perf_yellow) ? '' : $perf_yellow = 'false';
-    isset($perf_red) ? '' : $perf_red = 'false';
-    isset($monitor_up) ? '' : $monitor_up = 'false';
-    isset($monitor_down) ? '' : $monitor_down = 'false';
+    isset($seo) ? '' : $seo = 'false';
+    isset($a11y) ? '' : $a11y = 'false';
+    isset($perf) ? '' : $perf = 'false';
+    isset($monitor) ? '' : $monitor = 'false';
 @endphp
 
 <!DOCTYPE html>
@@ -28,62 +21,59 @@
         <link rel="stylesheet" href="/css/app.css" }}>
         <link rel="stylesheet" href="/tailwind/app.css" }}>
     </head>
-    <body class="bg-gray-900 font-body props-body">
-        <div class="nav bg-gray-800 py-6 lg:py-12 px-2 lg:px-8">
-            <div class="nav__icons">
-                <a class="text-2xl font-bold" href="/">NUprops</a>
-                <div class="nav__handle">
-                    <!-- TODO: ADD SEARCH ICON -->
-                    <svg class="nav__burger " width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <line y1="0.5" x2="21" y2="0.5" stroke="#EFEFEF"/>
-                        <line y1="7.5" x2="21" y2="7.5" stroke="#EFEFEF"/>
-                        <line y1="14.5" x2="21" y2="14.5" stroke="#EFEFEF"/>
-                    </svg>
-                </div>
-            </div>
-            <nav id="navigation" data-navigation-handle=".nav__handle" role="navigation">
-                <form action="/search" method="POST" role="search">
-                    @csrf
-                    <input class="my-16" name="search_title" type="text" placeholder="Search Properties"
-                    value="{{$q}}">
-
-                    <div class="nav__filters mb-8">
-                        <div class="mb-6">
-                            <h2 class="text-white font-medium">Uptime</h2>
-                            <label class="block"><input name="monitor" type="radio" value="up" {{$monitor_up != 'false' ? "checked" : ''}}>Up</label>
-                            <label class="block"><input name="monitor" type="radio" value="down" {{$monitor_down != 'false' ? "checked": ''}}>Down</label>
-                        </div>
-                        <div class="mb-6">
-                            <h2 class="text-white font-medium">Accessibility</h2>
-                            <label class="block"><input name="a11y" type="radio" value="green" {{$a11y_green != 'false' ? "checked" : ''}}>90-100</label>
-                            <label class="block"><input name="a11y" type="radio" value="yellow" {{$a11y_yellow != 'false' ? "checked" : ''}}>50-89</label>
-                            <label class="block"><input name="a11y" type="radio" value="red" {{$a11y_red != 'false' ? "checked" : ''}}>0-49</label>
-                        </div>
-
-                        <div class="mb-6">
-                            <h2 class="text-white font-medium">SEO</h2>
-                            <label class="block"><input name="seo" type="radio" value="green" {{$seo_green != 'false' ? "checked" : ''}}>90-100</label>
-                            <label class="block"><input name="seo" type="radio" value="yellow" {{$seo_yellow != 'false' ? "checked" : ''}}>50-89</label>
-                            <label class="block"><input name="seo" type="radio" value="red" {{$seo_red != 'false' ? "checked" : ''}}>0-49</label>
-                        </div>
-
-                        <div class="mb-6">
-                            <h2 class="text-white font-medium">Performance</h2>
-                            <label class="block"><input name="perf" type="radio" value="green" {{$perf_green != 'false' ? "checked" : ''}}>90-100</label>
-                            <label class="block"><input name="perf" type="radio" value="yellow" {{$perf_yellow != 'false' ? "checked" : ''}}>50-89</label>
-                            <label class="block"><input name="perf" type="radio" value="red" {{$perf_red != 'false' ? "checked" : ''}}>0-49</label>
-                        </div>
+    <body class="bg-gray-900 font-body flex flex-col lg:flex-row text-gray-200">
+        <div class="w-full lg:w-3/12 bg-gray-800 py-6 lg:py-10 px-2 lg:px-8">
+            <div class="lg:fixed lg:w-2/12">
+                <div class="flex items-center w-full">
+                    <a class="text-2xl font-bold" href="/">NUprops</a>
+                    <div class="nav__handle ml-auto lg:hidden">
+                        @include('svgs/menu')
                     </div>
-                    <button class="block w-full px-4 py-3 bg-gray-900 hover:bg-gray-700 text-white" type="submit">Filter</button>
-                </form>
-
-                <div class="pt-12">
-                    Dev Links: <a href="/prop">props</a> <a href="/org">orgs</a> <a href="/technology">techs</a> <a href="/user">users</a>
                 </div>
-            </nav>
+                <nav id="navigation" data-navigation-handle=".nav__handle" role="navigation">
+                    <form action="/search" class="bg-gray-800" method="POST" role="search">
+                        @csrf
+                        <input class="my-8 w-full bg-transparent py-2 px-1 border border-gray-400" name="search_title" type="text" placeholder="Search Properties"
+                        value="{{$q}}">
+
+                        <div class="nav__filters mb-8 text-gray-600">
+                            <div class="mb-6">
+                                <h2 class="text-white font-medium">Uptime</h2>
+                                <label class="block"><input class="mr-1" name="monitor" type="radio" value="up" {{$monitor == 'up' ? "checked" : ''}}>Up</label>
+                                <label class="block"><input class="mr-1" name="monitor" type="radio" value="down" {{$monitor == 'down' ? "checked": ''}}>Down</label>
+                            </div>
+                            <div class="mb-6">
+                                <h2 class="text-white font-medium">Accessibility</h2>
+                                <label class="block"><input class="mr-1" name="a11y" type="radio" value="green" {{$a11y == 'green' ? "checked" : ''}}>90-100</label>
+                                <label class="block"><input class="mr-1" name="a11y" type="radio" value="yellow" {{$a11y == 'yellow' ? "checked" : ''}}>50-89</label>
+                                <label class="block"><input class="mr-1" name="a11y" type="radio" value="red" {{$a11y == 'red' ? "checked" : ''}}>0-49</label>
+                            </div>
+
+                            <div class="mb-6">
+                                <h2 class="text-white font-medium">SEO</h2>
+                                <label class="block"><input class="mr-1" name="seo" type="radio" value="green" {{$seo == 'green' ? "checked" : ''}}>90-100</label>
+                                <label class="block"><input class="mr-1" name="seo" type="radio" value="yellow" {{$seo == 'yellow' ? "checked" : ''}}>50-89</label>
+                                <label class="block"><input class="mr-1" name="seo" type="radio" value="red" {{$seo == 'red' ? "checked" : ''}}>0-49</label>
+                            </div>
+
+                            <div class="mb-6">
+                                <h2 class="text-white font-medium">Performance</h2>
+                                <label class="block"><input class="mr-1" name="perf" type="radio" value="green" {{$perf == 'green' ? "checked" : ''}}>90-100</label>
+                                <label class="block"><input class="mr-1" name="perf" type="radio" value="yellow" {{$perf == 'yellow' ? "checked" : ''}}>50-89</label>
+                                <label class="block"><input class="mr-1" name="perf" type="radio" value="red" {{$perf == 'red' ? "checked" : ''}}>0-49</label>
+                            </div>
+                        </div>
+                        <button class="block w-full px-4 py-3 bg-gray-900 hover:bg-gray-700 text-white" type="submit">Filter</button>
+                        <button class="block w-full mt-2 px-4 py-3 bg-gray-700 hover:bg-gray-700 text-white" type="submit">Clear</button>
+                    </form>
+                </nav>
+            </div>
         </div>
 
         <main class="w-full pl-2 lg:pl-10 pt-12 pr-2 lg:pr-16 relative pb-6">
+            <div>
+                Dev Links: <a href="/prop">props</a> <a href="/org">orgs</a> <a href="/technology">techs</a> <a href="/user">users</a>
+            </div>
             @yield('content')
         </main>
     </body>
