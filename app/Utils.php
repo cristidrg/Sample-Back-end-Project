13e34b6d35d2688acbe33481d6a176510f5cab30
Provost Digital <pods@Provosts-MacBook-Pro-2.local>
2019-11-04 10:32:14 -0500

    Fix audit discrepancy bug

--- app/Utils.php
+++ app/Utils.php
@@ -24,4 +24,24 @@ class Utils {
             return 'digit-1';
         }
     }
+
+    public static function delete_directory($dirname) {
+        if (is_dir($dirname)) {
+            $dir_handle = opendir($dirname);
+            if (!$dir_handle)
+                return false;
+            while($file = readdir($dir_handle)) {
+                if ($file != "." && $file != "..") {
+                    if (!is_dir($dirname."/".$file))
+                            unlink($dirname."/".$file);
+                    else
+                            Utils::delete_directory($dirname.'/'.$file);
+                }
+            }
+            closedir($dir_handle);
+            rmdir($dirname);
+
+            return true;
+        }
+    }
 }
