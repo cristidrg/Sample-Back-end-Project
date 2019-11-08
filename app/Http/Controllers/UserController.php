8bb1d1e2076a60ababda4262b3fc671eb1999f3e
Provost Digital <pods@Provosts-MacBook-Pro-2.local>
2019-11-07 20:41:12 -0500

    Add validation for users and technologies too ;

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,14 +5,10 @@ namespace App\Http\Controllers;
 use App\User;
 use App\Org;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $users = User::all();
@@ -20,11 +16,6 @@ class UserController extends Controller
         return view('user/index', compact('users'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('user/create', [
@@ -32,67 +23,35 @@ class UserController extends Controller
         ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function store()
     {
-        $request->validate([
+        request()->validate([
             'first_name' => 'required',
             'last_name' => 'required',
             'title' => 'required',
-            'email' => 'required',
-            'maintains_orgs' => 'required'
+            'email' => 'required|unique:users',
+            'maintains_orgs' => 'required|array',
+            'maintains_orgs.*' => 'required|exists:orgs,title'
         ]);
 
-        if (User::where('email', $request->get('email'))->first() != null) {
-            return redirect('/user/create')->with('popup', 'Error: There is a user with this email');
-        }
+        $user = User::create(request(['first_name','last_name','title','email']));
 
-        $user = User::create([
-            'first_name' => $request->get('first_name'),
-            'last_name' => $request->get('last_name'),
-            'title' => $request->get('title'),
-            'email' => $request->get('email')
-        ]);
-
-        $orgs = $request->get('maintains_orgs');
-        if ($orgs && count($orgs) > 0) {
-            foreach ($orgs as $org) {
-                $orgModel = Org::where('title', $org)->first();
-                $user->orgs()->save($orgModel);
-            }
+        foreach (request('maintains_orgs') as $org) {
+            $user->orgs()->save(Org::where('title', $org)->first());
         }
 
         $user->save();
 
-        return redirect('user/')->with('popup', 'user ' . $request->get('first_name') . ' has been created!');
+        return redirect('user/')->with('popup', 'user ' . $request('first_name') . ' has been created!');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function edit(User $user)
     {
-        $user = User::find($id);
-
         return view('user/edit', [
             'user' => $user,
             'orgs' => Org::all(),
@@ -100,37 +59,23 @@ class UserController extends Controller
         ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function update(User $user)
     {
-        $request->validate([
+        request()->validate([
             'first_name' => 'required',
             'last_name' => 'required',
             'title' => 'required',
-            'email' => 'required',
-            'maintaining_orgs' => 'required'
+            'email' => ['required', Rule::unique('users')->ignore($user->id)],
+            'maintains_orgs' => 'required|array',
+            'maintains_orgs.*' => 'required|exists:orgs,title'
         ]);
 
-        $user = User::find($id);
-        $user->first_name = $request->get('first_name');
-        $user->last_name = $request->get('last_name');
-        $user->email = $request->get('email');
-        $user->title = $request->get('title');
+        $user->update(request(['first_name','last_name','email','title']));
         
-        $orgs = $request->get('maintaining_orgs');
-        if ($orgs && count($orgs) > 0) {
-            $user->orgs()->detach();
-
-            foreach ($orgs as $org) {
-                $orgModel = Org::where('title', $org)->first();
-                $user->orgs()->attach($orgModel);
-            }
+        $user->orgs()->detach();
+        foreach (request('maintains_orgs') as $org) {
+            $orgModel = Org::where('title', $org)->first();
+            $user->orgs()->attach($orgModel);
         }
 
         $user->save();
@@ -138,15 +83,8 @@ class UserController extends Controller
         return redirect('/user')->with('popup', 'user updated!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        $user = User::find($id);
         $user->delete();
 
         return redirect('/user')->with('popup', 'user has been deleted');
--- resources/views/org/edit.blade.php
+++ resources/views/org/edit.blade.php
@@ -4,6 +4,13 @@
 
 @section('content')
     <div>
+        @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
         <form method="post" action={{ route('org.update', $org->id) }}>
             @method('PATCH') 
             @csrf
