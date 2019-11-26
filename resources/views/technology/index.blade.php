eb7b6d6a6fbffd8c27eae415e6bc8cc081e3b562
Provost Digital <pods@Provosts-MacBook-Pro-2.local>
2019-11-26 11:47:21 -0500

    Style Technology and User Lists

--- resources/views/technology/index.blade.php
+++ resources/views/technology/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts/default')
+@extends('layouts/tables')
 
 @section('title', 'Technology List')
 
@@ -6,14 +6,18 @@
     @if(session()->get('popup'))
         {{ session()->get('popup') }}  
     @endif
-    <div>
-        <a href="{{ route('technology.create') }}"  class="text-xl">Create</a>
-        <table class="table table-striped">
-            <thead>
+    <div class="flex lg:inline-flex flex-col flex-wrap">
+        <div class="flex items-center mb-2 w-full lg:pr-2">
+            <h1 class="text-lg lg:text-3xl font-bold">Technologies</h1>
+            <a class="px-2 lg:px-4 py-1 ml-5 bg-green-500 rounded-full text-xs hover:bg-green-600 lg:text-sm" href="{{ route('technology.create') }}"">Create New Technology</a>
+        </div>
+        <div class="mobile-only:overflow-auto mobile-only:w-full pr-2">
+            <table class="table table-auto table-striped table-responsive text-sm props-table">
+                <thead>
                 <tr>
                     <td>ID</td>
                     <td>Name</td>
-                    <td colspan = 2>Actions</td>
+                    <td>Actions</td>
                 </tr>
             </thead>
             <tbody>
@@ -21,19 +25,18 @@
                 <tr>
                     <td>{{$technology->id}}</td>
                     <td>{{$technology->name}}</td>
-                    <td>
-                        <a href="{{ route('technology.edit',$technology->id)}}"  class="text-xl">Edit</a>
-                    </td>
-                    <td>
+                    <td class="flex py-1 text-sm justify-around">
+                        <a href="{{ route('technology.edit',$technology->id)}}" class="px-2 mr-2 hover:bg-blue-600 bg-blue-500 rounded-full">Edit</a>
                         <form action="{{ route('technology.destroy', $technology->id)}}" method="post">
                             @csrf
                             @method('DELETE')
-                            <button  class="text-xl" type="submit">Delete</button>
+                            <button class="px-2 hover:bg-red-600 bg-red-500 rounded-full" type="submit">Delete</button>
                         </form>
                     </td>
                 </tr>
                 @endforeach
             </tbody>
-        </table>
+            </table>
+        </div>
     </div>
 @stop
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts/default')
+@extends('layouts/tables')
 
 @section('title', 'User List')
 
@@ -6,46 +6,49 @@
     @if(session()->get('popup'))
         {{ session()->get('popup') }}  
     @endif
-    <div>
-        <a href="{{ route('user.create') }}" class="text-xl">Create</a>
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <td>ID</td>
-                    <td>First Name</td>
-                    <td>Last Name</td>
-                    <td>Email</td>
-                    <td>Title</td>
-                    <td>Orgs</td>
-                    <td colspan = 2>Actions</td>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($users as $user)
-                <tr>
-                    <td>{{$user->id}}</td>
-                    <td>{{$user->first_name}}</td>
-                    <td>{{$user->last_name}}</td>
-                    <td>{{$user->email}}</td>
-                    <td>{{$user->title}}</td>
-                    <td> 
-                        @foreach ($user->orgs as $org)
-                            {{$org->id}} 
-                        @endforeach
-                    </td>
-                    <td>
-                        <a href="{{ route('user.edit',$user->id)}}"  class="text-xl">Edit</a>
-                    </td>
-                    <td>
-                        <form action="{{ route('user.destroy', $user->id)}}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <button  class="text-xl" type="submit">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+    <div class="flex lg:inline-flex flex-col flex-wrap">
+        <div class="flex items-center mb-2  mobile-only:w-full lg:pr-2">
+            <h1 class="text-lg lg:text-3xl font-bold">NUProps Users</h1>
+            <a class="px-2 lg:px-4 py-1 ml-auto bg-green-500 rounded-full text-xs hover:bg-green-600 lg:text-sm" href="{{ route('user.create') }}"">Create New User</a>
+        </div>
+        <div class="mobile-only:overflow-auto mobile-only:w-full pr-2">
+            <table class="table table-auto table-striped table-responsive text-sm props-table">
+                <thead>
+                    <tr>
+                        <td>ID</td>
+                        <td>First Name</td>
+                        <td>Last Name</td>
+                        <td>Email</td>
+                        <td>Title</td>
+                        <td>Orgs</td>
+                        <td>Actions</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($users as $user)
+                    <tr>
+                        <td>{{$user->id}}</td>
+                        <td>{{$user->first_name}}</td>
+                        <td>{{$user->last_name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>{{$user->title}}</td>
+                        <td> 
+                            @foreach ($user->orgs as $org)
+                                {{$org->id}} 
+                            @endforeach
+                        </td>
+                        <td class="flex py-1 text-sm justify-around">
+                            <a href="{{ route('user.edit',$user->id)}}" class="px-2 mr-2 hover:bg-blue-600 bg-blue-500 rounded-full">Edit</a>
+                            <form action="{{ route('user.destroy', $user->id)}}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button class="px-2 hover:bg-red-600 bg-red-500 rounded-full" type="submit">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @stop
